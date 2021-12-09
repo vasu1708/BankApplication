7de18aaa96c802bae4e63ddb2797f0448a8be4f0
Srinivasa Reddy Bala <balasrinivas1708@gmail.com>
2021-12-09 12:46:10 +0530

    removed unsused namespaces

--- BankApp/BankApp.Services/DatabaseService.cs
+++ BankApp/BankApp.Services/DatabaseService.cs
@@ -1,10 +1,7 @@
-﻿using System;
+﻿using BankApp.Models;
 using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using BankApp.Models;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankApp.Services
 {
